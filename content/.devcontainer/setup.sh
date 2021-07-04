88fdbc54dc45dde413664accb18ab32f4be3644f
Justin Yoo <justin.yoojh@gmail.com>
2021-07-04 21:07:53 +0900

    Add Codespaces settings

---
+++ content/.devcontainer/setup.sh
@@ -0,0 +1,31 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Install node.js 14.x LTS
+nvm install --lts=fermium
+
+# Install Yarn
+npm install -g yarn
+
+# Install Gatsby
+npm install -g gatsby-cli
+
+# Install BooGi
+npm install -g boogi-cli
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
