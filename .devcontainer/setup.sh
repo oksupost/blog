1163c76fc0394f178a7f352cab41d561e77b2eb5
Justin Yoo <justin.yoojh@gmail.com>
2021-07-04 23:04:05 +0900

    Update Codespaces settings

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,26 +11,12 @@ apt-get install -y \
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
+# Install node.js 14.x LTS
+nvm install --lts=fermium
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
-# Install node.js 14.x LTS
-nvm install --lts=fermium
-nvm use --lts=fermium
-
-# Install oh-my-zsh plugins
-npm config set ignore-scripts false
-npm install -g spaceship-prompt --unsafe-perm
-
-# Install Yarn
-npm install -g yarn --unsafe-perm
-
-# Install Gatsby
-npm install -g gatsby-cli --unsafe-perm
-
-# Install BooGi
-npm install -g boogi-cli --unsafe-perm
---
+++ install-plugins.sh
@@ -0,0 +1,15 @@
+# Use the npm 14.x LTS
+nvm use --lts=fermium
+
+# Install oh-my-zsh plugins
+npm install -g spaceship-prompt --unsafe-perm
+source ~/.zshrc
+
+# Install Yarn
+npm install -g yarn --unsafe-perm
+
+# Install Gatsby
+npm install -g gatsby-cli --unsafe-perm
+
+# Install BooGi
+npm install -g boogi-cli --unsafe-perm
